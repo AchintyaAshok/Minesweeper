c146bc5838859fcbc5d68d9905e5fe2d4fff3a0f
Candidate <candidate@Jacks-MacBook-Air.local>
2016-06-30 12:28:20 -0400

    Finally got the board to dynamically load elements based on a randomized board setup.

--- modules/minesweeperApp/controllers/home.js
+++ modules/minesweeperApp/controllers/home.js
@@ -3,12 +3,21 @@
 var app = angular.module("minesweeper-app");
 app.controller("HomeController", ['$scope', function($scope){
 
-    $scope.score = 0;
+    $scope.score          = 0;
     $scope.secondsElapsed = 0;
+    $scope.boardSize      = 5; // this is the number of rows and columns.
+    $scope.boardElements  = [
+      ["1", "2", "x", "1", "2"],
+      ["3", "2", "x", "1", "2"],
+      ["1", "2", "x", "x", "2"],
+      ["2", "2", "x", "1", "2"],
+      ["1", "5", "x", "1", "2"]
+    ];
 
     function _init(){
       console.log("initialized home controller.");
-    }
+      console.log("Board Elements", $scope.boardElements);
+    };
 
     _init();
 }]);
