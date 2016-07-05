865c3252035efdd389de1409d8c223aa7efd0253
Achintya Ashok <aa3099@nyu.edu>
2016-07-04 21:54:41 -0400

    Some final stuff to go. Ok done.

--- modules/minesweeperApp/controllers/home.js
+++ modules/minesweeperApp/controllers/home.js
@@ -67,34 +67,10 @@ app.controller("HomeController", ['$scope', function($scope){
       // Post processing (ex. score valuation) after all the elements are exposed
     };
 
-    /* Checks the state of the board. If the position is not a mine, it will
-    then evaluate the position. This means:
-      1. It will display the value to the user
-      2. Evaluate whether to expand the surrounding area of the element.
-    */
-    $scope.evaluatePosition = function(rowPos, valPos)
-    {
-      console.log("Evaluating position:", {column: valPos, row: rowPos});
-      if($scope.exposedView[rowPos][valPos]){
-        console.log("Already exposed. We don't need to check this again.");
-        return;
-      }
-      ++$scope.movesMade;
-      if($scope.boardElements[rowPos][valPos] == MINE){
-        exposeEverything();
-        askForNewGame(false);
-        return;
-      }
-      expandPosition(rowPos, valPos); // expand around this position if all adjacent elements are alread viewable
-      exposeElement(rowPos, valPos);
-      checkBoardState();
-    };
-
     /* This will make all the itms around this position viewable if we are
     able to determine that a fenced area around this column is already visile. */
     function expandPosition(rowPos, valPos)
     {
-      console.log("Expanding the area around the position", rowPos, valPos);
       if(rowPos < 0 || rowPos >= $scope.boardElements.length){
         console.log("Beyond row bounds.");
         return;
@@ -122,6 +98,29 @@ app.controller("HomeController", ['$scope', function($scope){
       }
     };
 
+    /* Checks the state of the board. If the position is not a mine, it will
+    then evaluate the position. This means:
+      1. It will display the value to the user
+      2. Evaluate whether to expand the surrounding area of the element.
+    */
+    $scope.evaluatePosition = function(rowPos, valPos)
+    {
+      console.log("Evaluating position:", {column: valPos, row: rowPos});
+      if($scope.exposedView[rowPos][valPos]){
+        console.log("Already exposed. We don't need to check this again.");
+        return;
+      }
+      ++$scope.movesMade;
+      if($scope.boardElements[rowPos][valPos] == MINE){
+        exposeEverything();
+        askForNewGame(false);
+        return;
+      }
+      expandPosition(rowPos, valPos); // expand around this position if all adjacent elements are alread viewable
+      exposeElement(rowPos, valPos);
+      checkBoardState();
+    };
+
     /* Determines whether the user has won or not. */
     function checkBoardState()
     {
@@ -134,29 +133,23 @@ app.controller("HomeController", ['$scope', function($scope){
       // any post processing
     };
 
-    /* This will randomly generate a "size" x "size" board with the given size.
-    It will randomize a number of mines and place them. This will be a fraction
-    of the size of the board (depending on the difficulty). */
-    function generateBoard(numRows, numCols, difficulty)
+    /* Adjusts the numeric values around the given position where a mine has
+    been placed. */
+    function adjustAroundPosition(gridWithMines, row, col)
     {
-      console.log("Playing with difficulty: ", difficulty);
-      var numberOfElements = numRows*numCols;
-      var numberOfMines = Math.ceil(PROPORTION_OF_MINES[difficulty] * numberOfElements);
-      console.log("Number of mines -> ", numberOfMines);
-      $scope.numRows = numRows;
-      $scope.numCols = numCols;
-      $scope.numMines = numberOfMines;
-      $scope.boardSize = numberOfElements;
-      $scope.boardElements = placeMinesRandomly(numberOfMines);
-      $scope.exposedView = [];
-      for(var i=0; i<$scope.numRows; i++){
-        var row = [];
-        for(var j=0; j<$scope.numCols; j++){
-          row.push(false);
+      if(gridWithMines[row][col] != MINE) return;
+      for(var i=row-1; i<=row+1; i++){
+        for(var j=col-1; j<=col+1; j++){
+          if(i<0 || i>=gridWithMines.length || j<0 || j>= gridWithMines[i].length){
+            // out of bounds of evaluation
+            continue;
+          }
+          if(i==row && j==col) continue;
+          if(gridWithMines[i][j] == MINE) continue;
+          gridWithMines[i][j] += 1; // increase it by 1
         }
-        $scope.exposedView.push(row);
       }
-      console.log("Grid With Mines -> ", $scope.boardElements);
+      return gridWithMines;
     };
 
     /* Places all the mines randomly on the board. */
@@ -188,23 +181,29 @@ app.controller("HomeController", ['$scope', function($scope){
       return gridWithMines;
     };
 
-    /* Adjusts the numeric values around the given position where a mine has
-    been placed. */
-    function adjustAroundPosition(gridWithMines, row, col)
+    /* This will randomly generate a "size" x "size" board with the given size.
+    It will randomize a number of mines and place them. This will be a fraction
+    of the size of the board (depending on the difficulty). */
+    function generateBoard(numRows, numCols, difficulty)
     {
-      if(gridWithMines[row][col] != MINE) return;
-      for(var i=row-1; i<=row+1; i++){
-        for(var j=col-1; j<=col+1; j++){
-          if(i<0 || i>=gridWithMines.length || j<0 || j>= gridWithMines[i].length){
-            // out of bounds of evaluation
-            continue;
-          }
-          if(i==row && j==col) continue;
-          if(gridWithMines[i][j] == MINE) continue;
-          gridWithMines[i][j] += 1; // increase it by 1
+      console.log("Playing with difficulty: ", difficulty);
+      var numberOfElements = numRows*numCols;
+      var numberOfMines = Math.ceil(PROPORTION_OF_MINES[difficulty] * numberOfElements);
+      console.log("Number of mines -> ", numberOfMines);
+      $scope.numRows = numRows;
+      $scope.numCols = numCols;
+      $scope.numMines = numberOfMines;
+      $scope.boardSize = numberOfElements;
+      $scope.boardElements = placeMinesRandomly(numberOfMines);
+      $scope.exposedView = [];
+      for(var i=0; i<$scope.numRows; i++){
+        var row = [];
+        for(var j=0; j<$scope.numCols; j++){
+          row.push(false);
         }
+        $scope.exposedView.push(row);
       }
-      return gridWithMines;
+      console.log("Grid With Mines -> ", $scope.boardElements);
     };
 
     /* Sets the difficulty for the game. */
@@ -227,18 +226,6 @@ app.controller("HomeController", ['$scope', function($scope){
       $scope.numRows = numRows;
       $scope.numCols = numCols;
       $scope.selectedBoardSize = true;
-    };
-
-    /* Generates an alert that shows and then hides after a few seconds. */
-    function generateAlert(alertType, alertMessage)
-    {
-      console.log("Generating Alert", alertType, alertMessage);
-      $scope.alertType = alertType;
-      $scope.alertMessage = alertMessage;
-      $('#main-alert').show();
-      setTimeout(function(){
-        $('#main-alert').hide('fast');
-      }, 5000);
     };
 
     /* Prompts users for a new game */
@@ -275,11 +262,24 @@ app.controller("HomeController", ['$scope', function($scope){
       $scope.isGameSetup = true;
     };
 
+    /* Generates an alert that shows and then hides after a few seconds. */
+    function generateAlert(alertType, alertMessage)
+    {
+      console.log("Generating Alert", alertType, alertMessage);
+      $scope.alertType = alertType;
+      $scope.alertMessage = alertMessage;
+      $('#main-alert').show();
+      setTimeout(function(){
+        $('#main-alert').hide('fast');
+      }, 5000);
+    };
+
     /* Somewhat like a constructor for the controller. */
     function _init()
     {
       console.log("initialized home controller.");
       $('#main-alert').hide();
+      generateAlert("alert-info", "Setting it all up :D")
       $scope.newGame();
     };
 
